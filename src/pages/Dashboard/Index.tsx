77bd4809fa0fe4539cda32026ce9adc48d92e066
Matheus Santon Galter <matheussantong@gmail.com>
2022-11-13 18:04:05 -0300

    actual first commit

---
+++ src/pages/Dashboard/Index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { View, Text } from 'react-native'
+
+
+export default function Dashboard() {
+
+    return (
+        <View>
+            <Text>Tela dashboard</Text>
+        </View>
+    )
+
+}
